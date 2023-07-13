f7c27ba9b6acfab52434c7d7c7f05d559fa230f9
Dmitriy-OLW <124800644+Dmitriy-OLW@users.noreply.github.com>
2023-07-13 12:36:10 +0300

    Add files via upload

---
+++ LeftOsnova.js
@@ -0,0 +1,12 @@
+document.getElementById("leftcol").innerHTML = '\
+<font color="#a5260a" ><strong>Основное:</strong></font>\
+<hr>\
+<p><a href="B_Almanac.html"  style="text-decoration: none">Альманах</a></p>\
+<p><a href="LA_Book.html"  style="text-decoration: none">Книга</a></p>\
+<hr>\
+<p><a href="B_First_Game.html"  style="text-decoration: none">Игра по вселенной ордена</a></p>\
+<hr>\
+<p><a href="B_Text_Role.html"  style="text-decoration: none">Текстовые ролёвки</a></p>\
+<hr>\
+<p><a href="B_Rules.html"  style="text-decoration: none">Правила Приёма в Орден</a></p>\
+<p><a href="B_Contacts.html"  style="text-decoration: none">Связаться с нами</a></p>'
